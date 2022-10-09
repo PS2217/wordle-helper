18874355cf3afeeff3e34f4dff4dd86ee03aeac2
shawnp <shawnponce04@gmail.com>
2022-10-10 00:23:13 +1100

    added Suggestion UI but needs abit more styling

--- src/components/Suggestion/Suggestion.js
+++ src/components/Suggestion/Suggestion.js
@@ -1,11 +1,42 @@
 import React from 'react';
-
+import allWords from "./words.js";
 import './Suggestion.css';
 
+
+
+
+
+
 const Suggestion = props => {
+
+    let table1; 
+
+            if (props.text === "suggestUI") {}
+            table1 =
+            <div className='wordsTable'>
+                <table>
+                    <thead>
+                    </thead>
+                    <tbody>
+                        {allWords.map((allWords) => (
+                            <tr>
+                            <td>
+                              {allWords}
+                            </td>
+                            </tr>
+                        ))}
+                    </tbody>
+
+                </table>
+            </div>
+
+
     return (
         <React.Fragment>
-            
+
+        {table1}
+
+
         </React.Fragment>
     );
 };

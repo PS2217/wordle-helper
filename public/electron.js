a3142fcf6aca184851d02d388def62a4afbb546d
May Oo <mayphyuoo1211@gmail.com>
2022-10-26 15:27:10 +1100

    code cleanup

--- public/electron.js
+++ public/electron.js
@@ -1,7 +1,5 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
-const url = require('url');
-const isDev = require('electron-is-dev');
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -16,17 +14,8 @@ function createWindow() {
         win.maximize();
     })
     
-    win.loadURL(
-        isDev
-            ? 'http://localhost:3000'
-            : `file://${path.join(__dirname, '../build/index.html')}`
-    );
-
-    // Open the DevTools.
-    if (isDev) {
-        win.webContents.openDevTools({ mode: 'detach' });
-    }
-
+    win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
+    
     app.on('window-all-closed', () => {
         if (process.platform !== 'darwin') {
             app.quit()
